04ddf0dcc520665c12bc763dc0cf56af32d7c361
atsuiiw <panwgo@gmail.com>
2026-09-12 19:40:19 +0700

    Update Methods Available

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const FRONTEND_URL = process.env.FRONTEND_URL;
 const app = express();
 app.use(cors({
   origin: FRONTEND_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET'],
   credentials: true,
 }));
 app.use(express.json());
